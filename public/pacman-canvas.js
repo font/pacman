eeaef284c596c14ce5fd5c0c99ff443be0bdb66e
Ivan Font <ifont@redhat.com>
2017-03-30 22:59:31 -0700

    Wait no more than 10 seconds for cloud metadata

--- public/pacman-canvas.js
+++ public/pacman-canvas.js
@@ -99,6 +99,7 @@ function geronimo() {
             datatype: "json",
             type: "GET",
             url: "loc/metadata/get",
+            timeout: 10000, // wait no more than 10 seconds
             success: function(msg){
                 $(".cloudprovider").append("<b>" + msg['cloud'] + "</b>");
                 game.cloudProvider = msg['cloud'];
